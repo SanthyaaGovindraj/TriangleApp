3530e3d4e0bb9f87b9a9059b8051c4ef2a5c2a5c
SanthyaaGovindraj <84975677+SanthyaaGovindraj@users.noreply.github.com>
2021-06-28 02:06:44 +0530

    TriangleApp_2
    The class Triangle solver is created with the method Analyzer().

---
+++ TriangleSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleApp
+{
+    class TriangleSolver
+    {
+        public static int Analyze(int a, int b, int c)
+        {
+            int A = a, B = b, C = c;
+            string result;
+            if (A == B && B== C) 
+            {
+                result = "This is an Equilateral Triangle.";
+            
+            }
+            else if (A == B || A == C || B == C)
+            {
+                result = "This is an Isoceles Triangle.";
+
+            }
+            else 
+            {
+                result = "This is an Scalene Triangle.";
+
+            }
+
+            return Convert.ToInt32(result);
+        }
+
+
+
+    }
+}
